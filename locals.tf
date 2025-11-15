a7d8724c9b1b91378458d032f7f38737a049ce29
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-11-15 21:12:14 +0900

    fet

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals {
+  project_id  = "cloudrun-hands-on-473403"
+  region      = "asia-northeast1"
+  environment = "dev"
+  app_name    = "cnsrun"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  default = "cloudrun-hands-on-473403"
+}
+
+variable "region" {
+  default = "asia-northeast1"
+}
+
+variable "environment" {
+  default = "dev"
+}
